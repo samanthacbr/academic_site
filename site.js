de33d71441b0bbd24449f7afaa8ef4c2fd3adf7f
samanthacbr <35648061+samanthacbr@users.noreply.github.com>
2026-04-18 23:22:51 -0400

    Delete site.js

--- site.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-if (menuToggle && navLinks) {
-    menuToggle.addEventListener("click", () => {
-        const isOpen = navLinks.classList.toggle("is-open");
-        menuToggle.setAttribute("aria-expanded", String(isOpen));
-    });
-}
